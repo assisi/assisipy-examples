be476933338c846076a58e5df8290bea7d8480df
Damjan Miklic <damjan.miklic@gmail.com>
2016-06-21 23:45:03 +0200

    Debugging

--- deployment/fish_virtual_bee_simple/controllers/real_casu.py
+++ deployment/fish_virtual_bee_simple/controllers/real_casu.py
@@ -21,10 +21,10 @@ if __name__ == '__main__':
     while(1):
         temp = c.get_temp(casu.TEMP_WAX)
         if temp > 32:
-            c.set_diagnostic_led_rgb(r=1)
+            print('Sending cooking order!')
             c.send_message('virtual','cook')
         elif temp < 29:
-            c.set_diagnostic_led_rgb(b=1)
+            print('Sending cooling order!')
             c.send_message('virtual','cool')
 
         msg = c.read_message()
@@ -32,9 +32,11 @@ if __name__ == '__main__':
             if 'virtual' in msg.keys():
                 data = msg['virtual']
                 if data == 'cook':
+                    c.set_diagnostic_led_rgb(r=1)
                     print('Cooking to {0}C'.format(cook_ref))
                     c.set_temp(cook_ref)
                 elif data == 'cool':
+                    c.set_diagnostic_led_rgb(b=1)
                     print('Cooling to {0}C'.format(cool_ref))
                     c1.set_temp(cool_ref)
 
