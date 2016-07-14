bea9ff111143716259042be05ba19d6368bc664e
Damjan Miklic <damjan.miklic@gmail.com>
2016-07-14 23:36:27 +0200

    Updated vibration tests with pattern generation API.

--- actuator_testing/speaker_testing/bbg-002/controllers/vibration_pattern.py
+++ actuator_testing/speaker_testing/bbg-002/controllers/vibration_pattern.py
@@ -10,36 +10,26 @@ from assisipy import casu
 if __name__ == '__main__':
 
 
-    parser = argparse.ArgumentParser(description="Test casu heat actuator.")
+    parser = argparse.ArgumentParser(description="Test casu vibration patterns.")
     parser.add_argument('rtc_file', help='Name of the Casu rtc file.')
     args = parser.parse_args()
 
-    T_experiment = 1800 # Total experiment duration (1h)
+    T_experiment = 1800 # Total experiment duration (1/2h)
 
     mycasu = casu.Casu(args.rtc_file)
-    
-    time_start = time.time()
-    time_cycle_start_temp = time_start
-    time_now = time.time()
-    t_on = 1.0  # vibration on
-    t_off = 0.1 # vibration off
-    state = 0
-    vibe_counter = 0
-    while time_now - time_start < T_experiment:
-        t_loop_start = time.time();
-        if state == 0:
-            state = 1
-            vibe_amp = 50
-            vibe_freq = 440
-            t_delay = t_on
-        else:
-            state = 0
-            vibe_amp = 0
-            vibe_freq = 440
-            t_delay = t_off
 
-        mycasu.set_speaker_vibration(freq=vibe_freq,intens=vibe_amp)
-        time_now = time.time()
-        time.sleep(t_delay - (time.time() - t_loop_start))
-    mycasu.set_speaker_vibration(freq=0,intens=0)
+    vibe_periods = [1000,100]
+    vibe_freqs = [440,1]
+    vibe_amps = [50,0]
+    
+    print('{0} setting vibration pattern {1},{2},{3}'.format(mycasu.name(),
+                                                             vibe_periods,
+                                                             vibe_freqs,
+                                                             vibe_amps))
+    time_start = time.time()
+    mycasu.set_vibration_pattern(vibe_periods,vibe_freqs,vibe_amps)
+    time.sleep(T_experiment)
+    mycasu.speaker_standby()
+    print(mycasu.name() + ' done!')
+
     
--- actuator_testing/speaker_testing/bbg-004/controllers/vibration_pattern.py
+++ actuator_testing/speaker_testing/bbg-004/controllers/vibration_pattern.py
@@ -10,34 +10,26 @@ from assisipy import casu
 if __name__ == '__main__':
 
 
-    parser = argparse.ArgumentParser(description="Test casu heat actuator.")
+    parser = argparse.ArgumentParser(description="Test casu vibration patterns.")
     parser.add_argument('rtc_file', help='Name of the Casu rtc file.')
     args = parser.parse_args()
 
-    T_experiment = 3600 # Total experiment duration (1h)
+    T_experiment = 1800 # Total experiment duration (1/2h)
 
     mycasu = casu.Casu(args.rtc_file)
-    
-    time_start = time.time()
-    time_cycle_start_temp = time_start
-    time_now = time.time()
-    t_on = 1.0  # vibration on
-    t_off = 0.1 # vibration off
-    state = 0
-    vibe_counter = 0
-    while time_now - time_start < T_experiment:
-        if state == 0:
-            state = 1
-            vibe_amp = 100
-            vibe_freq = 440
-            t_delay = t_on
-        else:
-            state = 0
-            vibe_amp = 0
-            vibe_freq = 440
-            t_delay = t_off
 
-        mycasu.set_speaker_vibration(freq=vibe_freq,intens=vibe_amp)
-        print (time.time() - time_now)
-        time_now = time.time()
-        time.sleep(t_delay)
+    vibe_periods = [1000,100]
+    vibe_freqs = [440,1]
+    vibe_amps = [50,0]
+    
+    print('{0} setting vibration pattern {1},{2},{3}'.format(mycasu.name(),
+                                                             vibe_periods,
+                                                             vibe_freqs,
+                                                             vibe_amps))
+    time_start = time.time()
+    mycasu.set_vibration_pattern(vibe_periods,vibe_freqs,vibe_amps)
+    time.sleep(T_experiment)
+    mycasu.speaker_standby()
+    print(mycasu.name() + ' done!')
+
+    
